1f09f7097956dc36ce863451228b2144fa9eb0df
ikibis25 <74191485+ikibis25@users.noreply.github.com>
2020-11-09 16:11:58 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,259 @@
+/*---------------- Copyright (c) 2017 ----------------------------
+[Master Javascript]
+
+Template Name: ARTISTRY Responsive HTML Template
+Version: 1.0.0
+
+-------------------------------------------------------------------*/
+
+(function ($) {
+	"use strict";
+	
+	// on ready function
+	jQuery(document).ready(function($) {
+   		var $this = $(window);
+				
+		   $(".five li ul").hide();
+$(".five li:has('.submenu')").hover(
+  function(){
+  $(".five li ul").stop().fadeToggle(300);}
+);
+
+		// Loader js
+		$(window).on('load', function() {
+		$("#status").fadeOut();
+		$("#preloader").delay(300).fadeOut("slow")
+		});
+		
+		// Testimonial Slider Js
+		$(".kv_testimonial_wrapper .owl-carousel").owlCarousel({
+			loop:true,
+			items:1,
+			dots: true,
+			nav: false,
+			animateIn: 'fadeIn',
+			animateOut: 'fadeOut',
+			autoHeight: true,
+			touchDrag: false,
+			mouseDrag: false,
+			margin:0,
+			autoplay:true,
+			
+			navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+		});
+		
+		// Banner TYped  Js
+		$(function(){
+			$("#typed").typed({
+				stringsElement: $('#kv_bannertext'),
+				typeSpeed: 60,
+				backDelay: 1000,
+				loop: true,
+				contentType: 'html',
+				loopCount: false,
+				callback: function(){ foo(); },
+				resetCallback: function() { newTyped(); }
+			});
+			$(".reset").click(function(){
+				$("#typed").typed('reset');
+			});
+		});
+		function newTyped(){}
+		function foo(){}
+		
+	// Menu js
+	$("#menu_show").click(function(){
+		$("#menu_hide").slideToggle();
+	});
+	
+	//single page 
+	
+	var pluginName = 'ScrollIt',
+        pluginVersion = '1.0.3';
+
+    /*
+     * OPTIONS
+     */
+    var defaults = {
+        upKey: 38,
+        downKey: 40,
+        easing: 'linear',
+        scrollTime: 600,
+        activeClass: 'active',
+        onPageChange: null,
+        topOffset : 0
+    };
+
+    $.scrollIt = function(options) {
+
+        /*
+         * DECLARATIONS
+         */
+        var settings = $.extend(defaults, options),
+            active = 0,
+            lastIndex = $('[data-scroll-index]:last').attr('data-scroll-index');
+
+        /*
+         * METHODS
+         */
+
+        /**
+         * navigate
+         *
+         * sets up navigation animation
+         */
+        var navigate = function(ndx) {
+            if(ndx < 0 || ndx > lastIndex) return;
+
+            var targetTop = $('[data-scroll-index=' + ndx + ']').offset().top + settings.topOffset + 1;
+            $('html,body').animate({
+                scrollTop: targetTop,
+                easing: settings.easing
+            }, settings.scrollTime);
+        };
+
+        /**
+         * doScroll
+         *
+         * runs navigation() when criteria are met
+         */
+        var doScroll = function (e) {
+            var target = $(e.target).closest("[href]").attr('href') ||
+            $(e.target).closest("[data-scroll-goto]").attr('data-scroll-goto');
+            navigate(parseInt(target));
+        };
+
+        /**
+         * keyNavigation
+         *
+         * sets up keyboard navigation behavior
+         */
+        var keyNavigation = function (e) {
+            var key = e.which;
+            if($('html,body').is(':animated') && (key == settings.upKey || key == settings.downKey)) {
+                return false;
+            }
+            if(key == settings.upKey && active > 0) {
+                navigate(parseInt(active) - 1);
+                return false;
+            } else if(key == settings.downKey && active < lastIndex) {
+                navigate(parseInt(active) + 1);
+                return false;
+            }
+            return true;
+        };
+
+        /**
+         * updateActive
+         *
+         * sets the currently active item
+         */
+        var updateActive = function(ndx) {
+            if(settings.onPageChange && ndx && (active != ndx)) settings.onPageChange(ndx);
+
+            active = ndx;
+            $('[href]').removeClass(settings.activeClass);
+            $('[href=' + ndx + ']').addClass(settings.activeClass);
+        };
+
+        /**
+         * watchActive
+         *
+         * watches currently active item and updates accordingly
+         */
+        var watchActive = function() {
+            var winTop = $(window).scrollTop();
+
+            var visible = $('[data-scroll-index]').filter(function(ndx, div) {
+                return winTop >= $(div).offset().top + settings.topOffset &&
+                winTop < $(div).offset().top + (settings.topOffset) + $(div).outerHeight()
+            });
+            var newActive = visible.first().attr('data-scroll-index');
+            updateActive(newActive);
+        };
+
+        /*
+         * runs methods
+         */
+        $(window).on('scroll',watchActive).scroll();
+
+        $(window).on('keydown', keyNavigation);
+
+        $('.kv_main_manu').on('click','[href], [data-scroll-goto]', function(e){
+            e.preventDefault();
+            doScroll(e);
+        });
+
+    };
+	
+	// Contact Form Submition
+	function checkRequire(formId , targetResp){
+		targetResp.html('');
+		var email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+		var url = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
+		var image = /\.(jpe?g|gif|png|PNG|JPE?G)$/;
+		var mobile = /^[\s()+-]*([0-9][\s()+-]*){6,20}$/;
+		var facebook = /^(https?:\/\/)?(www\.)?facebook.com\/[a-zA-Z0-9(\.\?)?]/;
+		var twitter = /^(https?:\/\/)?(www\.)?twitter.com\/[a-zA-Z0-9(\.\?)?]/;
+		var google_plus = /^(https?:\/\/)?(www\.)?plus.google.com\/[a-zA-Z0-9(\.\?)?]/;
+		var check = 0;
+		$('#er_msg').remove();
+		var target = (typeof formId == 'object')? $(formId):$('#'+formId);
+		target.find('input , textarea , select').each(function(){
+			if($(this).hasClass('require')){
+				if($(this).val().trim() == ''){
+					check = 1;
+					$(this).focus();
+					targetResp.html('You missed out some fields.');
+					$(this).addClass('error');
+					return false;
+				}else{
+					$(this).removeClass('error');
+				}
+			}
+			if($(this).val().trim() != ''){
+				var valid = $(this).attr('data-valid');
+				if(typeof valid != 'undefined'){
+					if(!eval(valid).test($(this).val().trim())){
+						$(this).addClass('error');
+						$(this).focus();
+						check = 1;
+						targetResp.html($(this).attr('data-error'));
+						return false;
+					}else{
+						$(this).removeClass('error');
+					}
+				}
+			}
+		});
+		return check;
+	}
+	$(".submitForm").on("click", function() {
+		var _this = $(this);
+		var targetForm = _this.closest('form');
+		var errroTarget = targetForm.find('.response');
+		var check = checkRequire(targetForm , errroTarget);
+		if(check == 0){
+			var formDetail = new FormData(targetForm[0]);
+			formDetail.append('form_type' , _this.attr('form-type'));
+			$.ajax({
+				method : 'post',
+				url : 'ajax.php',
+				data:formDetail,
+				cache:false,
+				contentType: false,
+				processData: false
+			}).done(function(resp){
+				if(resp == 1){
+					targetForm.find('input').val('');
+					targetForm.find('textarea').val('');
+					errroTarget.html('<p style="color:green;">Mail has been sent successfully.</p>');
+				}else{
+					errroTarget.html('<p style="color:red;">Something went wrong please try again latter.</p>');
+				}
+			});
+		}
+	});
+	
+	});
+})();
